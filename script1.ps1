39ee6f5ba3d2f372623a2d65a011ae2b0f700df1
Sunil Kumar B S <47346540+sunilkumar03@users.noreply.github.com>
2023-05-22 17:09:50 +0530

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,1 +1,23 @@
+$sourceFile = "C:\path\*.bat"
 
+$idol = Read-Host "Enter the idol value (separated by commas if multiple):"
+$computers = @()
+foreach ($id in $idol -split ',') {
+    $computer = Get-ADComputer -Filter { Name -like $id } | Select-Object -ExpandProperty Name
+    if ($computer) {
+        $computers += $computer
+    }
+}
+
+$destinationPath = "\\$computer\D$\"
+
+foreach ($computer in $computers) {
+    $destination = $destinationPath + $computer
+    try {
+        Copy-Item -Path $sourceFile -Destination $destination -Force
+        Write-Host "File successfully copied to $destination"
+    }
+    catch {
+        Write-Host "Failed to copy file to $destination"
+    }
+}
